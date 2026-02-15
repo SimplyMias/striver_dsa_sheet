916dbbc28311cebf99e82114033df2aa1fe0cdbb
Saim Kazmi <saimkazmi017@gmail.com>
2026-02-15 14:33:26 +0530

    Initial commit

---
+++ input_output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hey Saim"<<endl;
+    cout<<"Hello Saim";
+    //to take input from the user we use cin 
+    int x; //let say a number x
+    cin >> x;
+    cout<<"You choose number: "<<x;
+    return 0;
+}
+
