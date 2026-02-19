9718716299d0ae4c59532d3e109fcac48c6423a1
Saim Kazmi <saimkazmi017@gmail.com>
2026-02-19 08:08:08 +0530

    Functions topic covered

---
+++ functions.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print_name() {
+        cout<<"Saim";
+    }
+void greeting(string name){
+    cout<< "Hey "<< name << endl;
+}
+float sum(float a, float b){
+    return a+b;
+}
+
+int main(){
+    // Functions are set of code which performs something for you 
+    // Functions are used to modularise code 
+    // Functions are used to increase readability 
+    // Functions are used to use same code multiple times 
+    // Types of Functions 
+    // void 
+    // return 
+    // parameterized 
+    // non parameterized 
+
+    // void functions - which does not returns anything 
+    // void print_name() {
+    //     cout<<"Saim";
+    // }
+    //Calling function from the main function 
+
+    // //print_name();
+    // string name;
+    // cin>>name;
+    // greeting(name);
+
+    // //parameterised functions
+    // string name;
+    // cin>>name;
+    // greeting(name);
+    
+
+    //Example 
+    //Take two numbers and prints its sum:
+    // float a;
+    // float b;
+    // cout<<"Enter a number: ";
+    // cin>>a;
+    // cout<<"Enter second number: ";
+    // cin>>b;
+    // float result = sum(a,b);
+    // cout<<"The sum of "<<a<<" and "<<b<<" is "<<result<<endl;
+
+
+    //Pass by value 
+    // it means 
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
